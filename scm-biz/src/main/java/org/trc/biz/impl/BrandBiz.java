2669760fa61308c15b03b38f91ffecb2d53737a1
dulongniu <376629441@qq.com>
2017-05-11 11:36:58 +0800

    属性管理后台代码提交，修改部分品牌管理后台代码

--- scm-biz/src/main/java/org/trc/biz/impl/BrandBiz.java
+++ scm-biz/src/main/java/org/trc/biz/impl/BrandBiz.java
@@ -7,22 +7,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.trc.biz.IBrandBiz;
+import org.trc.biz.qinniu.IQinniuBiz;
 import org.trc.domain.category.Brand;
 import org.trc.enums.CommonExceptionEnum;
 import org.trc.enums.ExceptionEnum;
 import org.trc.enums.ValidEnum;
+import org.trc.enums.ZeroToNineEnum;
 import org.trc.exception.CategoryException;
 import org.trc.exception.ParamValidException;
 import org.trc.form.BrandForm;
+import org.trc.form.FileUrl;
+import org.trc.service.IQinniuService;
 import org.trc.service.category.IBrandService;
 import org.trc.service.category.IPropertyService;
+import org.trc.util.BeanToMapUtil;
 import org.trc.util.CommonUtil;
 import org.trc.util.Pagenation;
 import org.trc.util.ParamsUtil;
 import tk.mybatis.mapper.entity.Example;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by hzqph on 2017/4/28.
@@ -34,8 +38,8 @@ public class BrandBiz implements IBrandBiz {
 
     @Autowired
     private IBrandService brandService;
-    @Autowired
-    private IPropertyService propertyService;
+//    @Autowired
+//    private IQinniuBiz qinniuBiz;
     @Override
     public Pagenation<Brand> brandPage(BrandForm queryModel, Pagenation<Brand> page) throws Exception {
         Example example=new Example(Brand.class);
@@ -54,7 +58,17 @@ public class BrandBiz implements IBrandBiz {
         }
         example.orderBy("isValid").desc();
         example.orderBy("updateTime").desc();
-        return brandService.pagination(example,page,queryModel);
+        Pagenation<Brand> pagenation=brandService.pagination(example,page,queryModel);
+//        //得到所有图片的缩略图,并以fileKey为key，url为value的形式封装成map
+//        List<Brand> brandList=pagenation.getResult();
+//        Map<String,String> fileUrlMap=constructFileUrlMap(brandList);
+//        for (Brand brand:brandList) {
+//            if(!StringUtils.isBlank(brand.getWebUrl())){
+//                brand.setWebUrl(fileUrlMap.get(brand.getWebUrl()));
+//            }
+//        }
+//        pagenation.setResult(brandList);
+        return pagenation;
     }
 
     @Override
@@ -134,4 +148,25 @@ public class BrandBiz implements IBrandBiz {
         return count;
     }
 
+//    private Map<String,String> constructFileUrlMap(List<Brand> brandList)throws Exception{
+//        Set<String> urlSet=new HashSet<>();
+//        for (Brand brand:brandList) {
+//            if(!StringUtils.isBlank(brand.getWebUrl())){
+//                urlSet.add(brand.getWebUrl());
+//            }
+//        }
+//        if(null!=urlSet&&urlSet.size()>0){
+//            String[] urlStr=new String[urlSet.size()];
+//            urlSet.toArray(urlStr);
+//            List<FileUrl> fileUrlList=qinniuBiz.batchGetFileUrl(urlStr, ZeroToNineEnum.ONE.getCode());
+//            if(null!=fileUrlList&&fileUrlList.size()>0){
+//                Map<String,String> fileUrlMap=new HashMap<>();
+//                for (FileUrl fileUrl :fileUrlList) {
+//                    fileUrlMap.put(fileUrl.getFileKey(),fileUrl.getUrl());
+//                }
+//                return fileUrlMap;
+//            }
+//        }
+//        return null;
+//    }
 }
