e280330fbca121cf4048136885c7c5342b0fc0a4
dulongniu <376629441@qq.com>
2017-05-15 15:08:33 +0800

    提交mybaties xml格式的sql语句写法文件

--- scm-dao/src/main/java/org/trc/mapper/supplier/ISupplierApplyMapper.java
+++ scm-dao/src/main/java/org/trc/mapper/supplier/ISupplierApplyMapper.java
@@ -1,8 +1,6 @@
 package org.trc.mapper.supplier;
 
-import org.apache.ibatis.annotations.SelectProvider;
 import org.trc.domain.supplier.SupplierApply;
-import org.trc.mapper.builder.SupplierApplyBuildSql;
 import org.trc.util.BaseMapper;
 
 import java.util.List;
@@ -13,9 +11,7 @@ import java.util.Map;
  */
 public interface ISupplierApplyMapper extends BaseMapper<SupplierApply>{
 
-    @SelectProvider(type = SupplierApplyBuildSql.class, method = "querySupplierApplyList")
     public List<SupplierApply> querySupplierApplyList(Map<String,Object> map);
 
-    @SelectProvider(type = SupplierApplyBuildSql.class, method = "queryCountSupplierApply")
     public int queryCountSupplierApply(Map<String,Object> map);
 }
