52d04ab5ac3d840bab4f888ec4085996c7127f20
szy <876619304@qq.com>
2017-05-23 21:47:17 +0800

    szy

---
+++ scm-biz/src/main/java/org/trc/service/category/ICategoryPropertyService.java
@@ -0,0 +1,16 @@
+package org.trc.service.category;
+
+import org.trc.domain.category.CategoryBrand;
+import org.trc.domain.category.CategoryBrandExt;
+import org.trc.domain.category.CategoryProperty;
+import org.trc.service.IBaseService;
+
+import java.util.List;
+
+/**
+ * Created by hzwdx on 2017/5/18.
+ */
+public interface ICategoryPropertyService extends IBaseService<CategoryProperty, Long>{
+
+
+}
