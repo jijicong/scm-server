bbf431b7ac8a369be399608314418e640180f007
sone21 <18155464310>
2017-06-06 14:20:25 +0800

    角色用户关系表的is_valid 字段操作

--- scm-biz/src/main/java/org/trc/service/impl/impower/UserAccreditInfoRoleRelationService.java
+++ scm-biz/src/main/java/org/trc/service/impl/impower/UserAccreditInfoRoleRelationService.java
@@ -9,6 +9,7 @@ import org.trc.service.impl.BaseService;
 import org.trc.service.impower.IUserAccreditInfoRoleRelationService;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sone on 2017/5/16.
@@ -20,13 +21,18 @@ public class UserAccreditInfoRoleRelationService extends BaseService<UserAccredi
     private UserAccreditRoleRelationMapper userAccreditRoleRelationMapper;
 
     @Override
-    public void deleteByUserAccreditId(Long userAccreditId) throws Exception {
+    public void deleteByUserAccreditId(Long userAccreditId){
         userAccreditRoleRelationMapper.deleteByUserAccreditId(userAccreditId);
-
     }
 
     @Override
-    public List<UserAccreditRoleRelation> selectListByUserAcId(Long userAccreditId) throws Exception {
+    public List<UserAccreditRoleRelation> selectListByUserAcId(Long userAccreditId){
         return userAccreditRoleRelationMapper.selectListByUserAcId(userAccreditId);
     }
+
+    @Override
+    public void updateStatusByRoleId(Map<String,Object> map) {
+        userAccreditRoleRelationMapper.updateStatusByRoleId(map);
+    }
+
 }
