e4e520e7c57b44d7a1b51d6621737084c837c966
xmn1986 <471869639@qq.com>
2017-07-04 12:48:39 +0800

    订单管理查询修改

--- scm-dao/src/main/java/org/trc/domain/order/OrderBase.java
+++ scm-dao/src/main/java/org/trc/domain/order/OrderBase.java
@@ -12,6 +12,14 @@ import java.util.Date;
  */
 public class OrderBase {
 
+    // 用户id
+    @Transient
+    private String userId;
+
+    // 会员名称
+    @Transient
+    private String userName;
+
     // 订单类型：0-普通订单 1-零元购 2-分期购 3-拼团
     @Transient
     private Byte type;
@@ -143,5 +151,21 @@ public class OrderBase {
 
     public void setShopMemo(String shopMemo) {
         this.shopMemo = shopMemo;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
