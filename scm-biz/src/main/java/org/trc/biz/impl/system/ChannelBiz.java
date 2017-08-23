8acd8d51b80bc672b30e51aacc4633450f860f45
sone21 <18155464310>
2017-08-23 14:53:56 +0800

    缓存更改

--- scm-biz/src/main/java/org/trc/biz/impl/system/ChannelBiz.java
+++ scm-biz/src/main/java/org/trc/biz/impl/system/ChannelBiz.java
@@ -73,7 +73,7 @@ public class ChannelBiz implements IChannelBiz {
 
 
     @Override
-    @Cacheable(key = "#form+#page+#page.pageNo+#page.pageSize" ,isList = true)
+    @Cacheable(key = "#form+#page.pageNo+#page.pageSize" ,isList = true)
     public Pagenation<Channel> channelPage(ChannelForm form, Pagenation<Channel> page) {
         Example example = new Example(Channel.class);
         Example.Criteria criteria = example.createCriteria();
