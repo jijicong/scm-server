3ea55357fee549f65a96af8ce4f6883ffc7cdd36
dulongniu <376629441@qq.com>
2017-09-01 17:39:03 +0800

    生产环境配置修改

--- scm-common/src/main/java/org/trc/spring/TrPropertyPlaceholderConfigurer.java
+++ scm-common/src/main/java/org/trc/spring/TrPropertyPlaceholderConfigurer.java
@@ -1,15 +1,22 @@
 package org.trc.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.trc.util.DESUtil;
 
+import java.util.Enumeration;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by george on 2016/11/16.
  */
 public class TrPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+    private final static Logger log = LoggerFactory.getLogger(TrPropertyPlaceholderConfigurer.class);
     private Properties props;
 
     public TrPropertyPlaceholderConfigurer() {
@@ -25,6 +32,34 @@ public class TrPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigur
 
     protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props) throws BeansException {
         this.props = props;
+        //将Property做改进
+        Enumeration<String> enumeration = (Enumeration<String>) props.propertyNames();
+
+        while (enumeration.hasMoreElements()) {
+            try {
+                String name = enumeration.nextElement();
+                String value = (String) props.get(name);
+                value = handleDecryptString(value);
+                props.setProperty(name, value);
+            } catch (Exception e) {
+                log.warn("properties初始化赋值异常,e:{},message:{}", e, e.getMessage());
+            }
+        }
         super.processProperties(beanFactoryToProcess, props);
+    }
+
+    /**
+     * 根据正则表达式匹配properties 中的值是否需要被解密
+     *
+     * @param value
+     * @return
+     */
+    private String handleDecryptString(String value) throws Exception {
+        Pattern p = Pattern.compile("!(.*?)!");
+        Matcher m = p.matcher(value);
+        while (m.find()) {
+            return DESUtil.decrypt(m.group(1), ConsumerConstant.DES_KEY);
+        }
+        return value;
     }
 }
---
+++ scm-common/src/main/java/org/trc/util/DESUtil.java
@@ -0,0 +1,145 @@
+package org.trc.util;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import java.io.IOException;
+import java.security.SecureRandom;
+
+/**
+ * DESUtil
+ */
+public final class DESUtil {
+    private DESUtil() {
+    }
+
+    private final static String DES = "DES";
+
+    public static void main(String[] args) throws Exception {
+        String data="Red2017Si";
+        String key = "86ea48faaf224965300d249835ff7ff8";
+        String s = encrypt(data, key);
+        System.out.println(s);
+        String a=decrypt(s,key);
+        System.out.println(a);
+        System.out.println(data.equals(a));
+    }
+    /**
+     * Description 根据键值进行加密
+     *
+     * @param data
+     * @param key  加密键byte数组
+     * @return
+     * @throws Exception
+     */
+    public static final String encrypt(String data, String key) throws Exception {
+        byte[] bt = encrypt(data.getBytes(), key.getBytes());
+        Base64 base64=new Base64();
+        bt=base64.encode(bt);
+        return new String(bt,"UTF-8");
+    }
+
+    /**
+     * Description 根据键值进行解密
+     *
+     * @param data
+     * @param key  加密键byte数组
+     * @return
+     * @throws IOException
+     * @throws Exception
+     */
+    public static final String decrypt(String data, String key) throws IOException, Exception {
+        if (data == null)
+            return null;
+
+        Base64 base64=new Base64();
+        byte[] buf=base64.decode(data);
+        /*BASE64Decoder decoder = new BASE64Decoder();
+        byte[] buf = decoder.decodeBuffer(data);*/
+        byte[] bt = decrypt(buf, key.getBytes());
+        return new String(bt,"UTF-8");
+    }
+
+    /**
+     *使用Base64加密算法加密字符串
+     *return
+     */
+    public static String encodeStr(String plainText){
+        byte[] b=plainText.getBytes();
+        Base64 base64=new Base64();
+        b=base64.encode(b);
+        String s=new String(b);
+        return s;
+    }
+
+    /**
+     *使用Base64加密
+     *return
+     */
+    public static String decodeStr(String encodeStr){
+        byte[] b=encodeStr.getBytes();
+        Base64 base64=new Base64();
+        b=base64.decode(b);
+        String s=new String(b);
+        return s;
+    }
+
+    /**
+     * Description 根据键值进行加密
+     *
+     * @param data
+     * @param key  加密键byte数组
+     * @return
+     * @throws Exception
+     */
+    private static final byte[] encrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        // Cipher对象实际完成加密操作
+        Cipher cipher = Cipher.getInstance(DES);
+
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
+
+        return cipher.doFinal(data);
+    }
+
+    /**
+     * Description 根据键值进行解密
+     *
+     * @param data
+     * @param key  加密键byte数组
+     * @return
+     * @throws Exception
+     */
+    private static final byte[] decrypt(byte[] data, byte[] key) throws Exception {
+        // 生成一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+
+        // 从原始密钥数据创建DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(key);
+
+        // 创建一个密钥工厂，然后用它把DESKeySpec转换成SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        // Cipher对象实际完成解密操作
+        Cipher cipher = Cipher.getInstance(DES);
+
+        // 用密钥初始化Cipher对象
+        cipher.init(Cipher.DECRYPT_MODE, securekey, sr);
+
+        return cipher.doFinal(data);
+    }
+}
