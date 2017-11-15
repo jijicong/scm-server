38a2c55c0d851b94b59fbabe5e5464be153c7e67
wangyuzhong <550670854@qq.com>
2017-11-15 16:27:49 +0800

    粮油报表导出代码修改

--- scm-biz/src/main/java/org/trc/biz/impl/liangyou/LiangYouBiz.java
+++ scm-biz/src/main/java/org/trc/biz/impl/liangyou/LiangYouBiz.java
@@ -288,7 +288,7 @@ public class LiangYouBiz implements ILiangYouBiz {
             lyStatement.setShopOrderCode(orderItem.getShopOrderCode());
             lyStatement.setSupplierOrderCode(orderItem.getSupplierOrderCode());
             lyStatement.setPayment(orderItem.getPayment());
-            lyStatement.setCreateTime(orderItem.getCreateTime());
+            lyStatement.setCreateTime(DateUtils.formatDateTime(orderItem.getCreateTime()));
             list.add(lyStatement);
         }
         return list;
@@ -414,10 +414,10 @@ public class LiangYouBiz implements ILiangYouBiz {
             CellDefinition skuCode = new CellDefinition("supplierSkuCode", "粮油商品SKU", CellDefinition.TEXT, 4000);
             CellDefinition itemName = new CellDefinition("itemName", "粮油商品名称", CellDefinition.TEXT, 8000);
             CellDefinition num = new CellDefinition("num", "交易数量", CellDefinition.NUM_0_00, 2000);
-            CellDefinition platformOrderCode = new CellDefinition("platformOrderCode", "平台订单号", CellDefinition.NUM_0_00, 4000);
+            CellDefinition platformOrderCode = new CellDefinition("platformOrderCode", "平台订单号", CellDefinition.TEXT, 4000);
             CellDefinition shopOrderCode = new CellDefinition("shopOrderCode", "店铺订单号", CellDefinition.TEXT, 4000);
             CellDefinition supplierOrderCode = new CellDefinition("supplierOrderCode", "粮油订单号", CellDefinition.TEXT, 4000);
-            CellDefinition payment = new CellDefinition("payment", "买家实付商品金额", CellDefinition.TEXT, 2000);
+            CellDefinition payment = new CellDefinition("payment", "买家实付商品金额", CellDefinition.NUM_0_00, 2000);
             CellDefinition createTime = new CellDefinition("createTime", "系统发送粮油时间", CellDefinition.TEXT, 8000);
 
             List<CellDefinition> cellDefinitionList = new ArrayList<>();
