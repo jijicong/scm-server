dfdadb5d441962900a69bf6c17c283a416799247
沈振宇 <hzszy@tairanchina.com>
2017-11-16 16:42:28 +0800

    查询销售渠道列表

--- scm-biz/src/main/java/org/trc/service/System/ISellChannelService.java
+++ scm-biz/src/main/java/org/trc/service/System/ISellChannelService.java
@@ -3,11 +3,16 @@ package org.trc.service.System;
 import org.trc.domain.System.SellChannel;
 import org.trc.service.IBaseService;
 
+import java.util.List;
+
 /**
  *
  * @author sone
  * @date 2017/5/2
  */
 public interface ISellChannelService extends IBaseService<SellChannel,Long>{
-
+    /**查询销售渠道列表
+     * @return
+     */
+    List<SellChannel> queryAllSellChannel();
 }
