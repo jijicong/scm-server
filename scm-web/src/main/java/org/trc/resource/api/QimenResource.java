689e58b686962fc5e4c6aaf7deda7845b6399458
沈振宇 <hzszy@tairanchina.com>
2017-11-24 11:37:54 +0800

    奇门测试

---
+++ scm-web/src/main/java/org/trc/resource/api/QimenResource.java
@@ -0,0 +1,35 @@
+package org.trc.resource.api;
+
+
+import com.alibaba.fastjson.JSON;
+import com.qimen.api.request.EntryorderConfirmRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.trc.constants.SupplyConstants;
+
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.xml.ws.Response;
+
+
+
+/**
+ * @author hzszy
+ * 提供给奇门的接口
+ */
+@Component
+@Path(SupplyConstants.Qimen.QI_MEN)
+
+public class QimenResource {
+    private Logger logger = LoggerFactory.getLogger(QimenResource.class);
+    @GET
+    @Path(SupplyConstants.Qimen.CONFIRM_INVOICE)
+    @Produces("application/json;charset=utf-8")
+    public Response confirmInvoice(@BeanParam EntryorderConfirmRequest confirmRequest) throws Exception{
+        logger.info(JSON.toJSONString(confirmRequest));
+        return null;
+    }
+}
