3d5219707a88781d071eb12e6f0aa3d62f5c3372
沈振宇 <hzszy@tairanchina.com>
2017-11-27 17:28:07 +0800

    1

--- scm-web/src/main/java/org/trc/resource/api/QimenResource.java
+++ scm-web/src/main/java/org/trc/resource/api/QimenResource.java
@@ -2,6 +2,8 @@ package org.trc.resource.api;
 
 
 import com.alibaba.fastjson.JSON;
+import com.qimen.api.DefaultQimenClient;
+import com.qimen.api.QimenClient;
 import com.qimen.api.request.EntryorderConfirmRequest;
 import org.apache.http.HttpRequest;
 import org.slf4j.Logger;
@@ -26,6 +28,7 @@ public class QimenResource {
     private Logger logger = LoggerFactory.getLogger(QimenResource.class);
     @POST
     @Path(SupplyConstants.Qimen.QIMEN_CALLBACK)
+    @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_XML)
     public Response confirmInvoice(@BeanParam EntryorderConfirmRequest confirmRequest, @Context HttpRequest request) throws Exception{
         logger.info(JSON.toJSONString(confirmRequest.getEntryOrder()));
@@ -35,6 +38,7 @@ public class QimenResource {
         logger.info(JSON.toJSONString(confirmRequest.getResponseClass()));
         logger.info(JSON.toJSONString(confirmRequest.getExtendProps()));
         logger.info(JSON.toJSONString(request));
+
         Response qimenResponse = new Response() ;
         qimenResponse.setFlag("success");
         qimenResponse.setCode("0");
