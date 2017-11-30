a5b6d77d928391c1590accd963c9f5b5ae2ab508
hzzyq <hzzyq@tairanchina.com>
2017-11-30 11:36:09 +0800

    1136

--- scm-dao/src/main/java/org/trc/domain/warehouseNotice/WarehouseNoticeDetails.java
+++ scm-dao/src/main/java/org/trc/domain/warehouseNotice/WarehouseNoticeDetails.java
@@ -77,9 +77,9 @@ public class WarehouseNoticeDetails {
     //收货状态
     private Integer status;
     //正品入库数量
-    private Integer normalStorageQuantity;
+    private Long normalStorageQuantity;
     //残次品入库数量
-    private Integer defectiveStorageQuantity;
+    private Long defectiveStorageQuantity;
     //货主编码
     private String ownerCode;
     //第三方仓库商品ID
@@ -195,19 +195,19 @@ public class WarehouseNoticeDetails {
 		this.status = status;
 	}
 
-	public Integer getNormalStorageQuantity() {
+	public Long getNormalStorageQuantity() {
 		return normalStorageQuantity;
 	}
 
-	public void setNormalStorageQuantity(Integer normalStorageQuantity) {
+	public void setNormalStorageQuantity(Long normalStorageQuantity) {
 		this.normalStorageQuantity = normalStorageQuantity;
 	}
 
-	public Integer getDefectiveStorageQuantity() {
+	public Long getDefectiveStorageQuantity() {
 		return defectiveStorageQuantity;
 	}
 
-	public void setDefectiveStorageQuantity(Integer defectiveStorageQuantity) {
+	public void setDefectiveStorageQuantity(Long defectiveStorageQuantity) {
 		this.defectiveStorageQuantity = defectiveStorageQuantity;
 	}
 
