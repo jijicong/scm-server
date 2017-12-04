6f121c351fc0b5ba9156752706df097faf87973f
沈振宇 <hzszy@tairanchina.com>
2017-12-04 10:38:27 +0800

    奇门相关

--- scm-web/src/main/java/org/trc/resource/api/QimenResource.java
+++ scm-web/src/main/java/org/trc/resource/api/QimenResource.java
@@ -51,7 +51,7 @@ public class QimenResource {
     private IQimenBiz qimenBiz;
     @POST
     @Path(SupplyConstants.Qimen.QIMEN_CALLBACK)
-    @Consumes(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_XML)
     public Response confirmInvoice(@Context HttpServletRequest request,@BeanParam QimenUrlRequest qimenUrlRequest){
         try {
