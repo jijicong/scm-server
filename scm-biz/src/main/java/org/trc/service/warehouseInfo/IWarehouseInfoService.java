0ad03d835b74c014b7419d4deea478aafdc161be
wangzhen369 <369073048@qq.com>
2017-12-05 17:46:21 +0800

    采购通知单校验提交

--- scm-biz/src/main/java/org/trc/service/warehouseInfo/IWarehouseInfoService.java
+++ scm-biz/src/main/java/org/trc/service/warehouseInfo/IWarehouseInfoService.java
@@ -3,8 +3,12 @@ package org.trc.service.warehouseInfo;
 import org.trc.domain.warehouseInfo.WarehouseInfo;
 import org.trc.service.IBaseService;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by wangyz on 2017/11/15.
  */
 public interface IWarehouseInfoService extends IBaseService<WarehouseInfo,Long> {
+    List<WarehouseInfo> selectWarehouseInfo(Map<String, String> map);
 }
--- scm-dao/src/main/java/org/trc/mapper/warehouseInfo/IWarehouseInfoMapper.java
+++ scm-dao/src/main/java/org/trc/mapper/warehouseInfo/IWarehouseInfoMapper.java
@@ -3,8 +3,12 @@ package org.trc.mapper.warehouseInfo;
 import org.trc.domain.warehouseInfo.WarehouseInfo;
 import org.trc.util.BaseMapper;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by wangyz on 2017/11/15.
  */
 public interface IWarehouseInfoMapper extends BaseMapper<WarehouseInfo> {
+    List<WarehouseInfo> selectWarehouseInfo(Map<String, String> map);
 }
