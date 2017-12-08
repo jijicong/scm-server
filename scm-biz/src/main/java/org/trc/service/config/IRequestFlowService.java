943ebbba8b27c9577456eba4753b5af263638ac1
hzzyq <hzzyq@tairanchina.com>
2017-12-08 15:05:32 +0800

    1505

--- scm-biz/src/main/java/org/trc/service/config/IRequestFlowService.java
+++ scm-biz/src/main/java/org/trc/service/config/IRequestFlowService.java
@@ -15,4 +15,15 @@ public interface IRequestFlowService extends IBaseService<RequestFlow,Long> {
     int updateRequestFlowByRequestNum(RequestFlow requestFlow);
 
     List<RequestFlow> queryBatch(QureyCondition condition);
+    
+    /**
+     * 插入请求流水记录
+     * @param requester  请求者
+     * @param responder  发送者
+     * @param type  请求类型
+     * @param status 状态
+     * @param reqParam 请求参数
+     * @return 请求流水
+     */
+    String insertRequestFlow(String requester, String responder, String type, String status, String reqParam);
 }
---
+++ scm-biz/src/test/java/org/trc/service/OutBoundNoticeChannelTest.java
@@ -0,0 +1,24 @@
+package org.trc.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.trc.biz.impl.outbound.OutBoundOrderBiz;
+
+@RunWith(SpringJUnit4ClassRunner.class)  //标记测试运行的环境
+@ContextConfiguration({"classpath:config/resource-context.xml"}) //配合spring测试  可以引入多个配置文件
+public class OutBoundNoticeChannelTest {
+
+	
+	@Autowired
+	OutBoundOrderBiz biz;
+//	private 
+	@Test
+	public void testNotice () throws Exception {
+		String reqStr = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><request><packages><package><expressCode>Y12323</expressCode><logisticsCode>SF</logisticsCode><logisticsName>顺丰</logisticsName><items><item><quantity>7</quantity><itemCode>111</itemCode></item><item><quantity>7</quantity><itemCode>110</itemCode></item></items></package></packages><deliveryOrder><deliveryOrderCode>FHTZ2017111112345</deliveryOrderCode><warehouseCode></warehouseCode><orderType>JYCK</orderType><operateTime>2017-11-11 11:11:11</operateTime></deliveryOrder></request>";
+	
+		biz.updateOutboundDetail(reqStr);
+	}
+}
