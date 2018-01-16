5b1b224517594ad0e243b0db7b2d1ba61415c1a7
吴东雄 <hzwdx@tairanchina.com>
2018-01-16 17:21:47 +0800

    订单商品状态枚举更新

--- scm-api/src/main/java/org/trc/enums/OrderItemDeliverStatusEnum.java
+++ scm-api/src/main/java/org/trc/enums/OrderItemDeliverStatusEnum.java
@@ -7,7 +7,8 @@ package org.trc.enums;
 public enum OrderItemDeliverStatusEnum {
     //WAIT_FOR_SUBMIT("1","待发送"),SUBMIT("2","已发送"),WAIT_FOR_DELIVER("3","待发货"),DELIVER("4","已发货"),SUBMIT_FAILURE("5","下单失败");
     ORDER_SPLITING("0","正在拆单"),WAIT_FOR_SUBMIT("1","待发送供应商"),ORDER_EXCEPTION("2","供应商下单异常"),WAIT_FOR_DELIVER("3","等待供应商发货"),ALL_DELIVER("4","全部发货"),
-    ORDER_FAILURE("5","供应商下单失败"),PARTS_DELIVER("6","部分发货"),ORDER_CANCEL("7","已取消");
+    ORDER_FAILURE("5","供应商下单失败"),PARTS_DELIVER("6","部分发货"),ORDER_CANCEL("7","已取消"),WAREHOUSE_RECIVE_FAILURE("8","仓库接收失败"),WAIT_WAREHOUSE_DELIVER("9","等待仓库发货"),
+    WAIT_HANDLER("10","待了结"),HANDLERED("11","已了结"),WAREHOUSE_MIDDEL_STATUS("12","仓库告知的过程中状态");
     public static OrderItemDeliverStatusEnum queryNameByCode(String code){
         for(OrderItemDeliverStatusEnum auditStatusEnum: OrderItemDeliverStatusEnum.values()){
             if (auditStatusEnum.getCode().equals(code)){
