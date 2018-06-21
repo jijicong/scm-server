fa94e876b8fadcb38c66d89d59ca3696a5503254
hzzyq <hzzyq@tairanchina.com>
2018-06-21 11:02:46 +0800

    1102

--- scm-biz/src/main/java/org/trc/biz/impl/order/ScmOrderBiz.java
+++ scm-biz/src/main/java/org/trc/biz/impl/order/ScmOrderBiz.java
@@ -3158,6 +3158,7 @@ public class ScmOrderBiz implements IScmOrderBiz {
         Example.Criteria criteria = example.createCriteria();
         criteria.andIn("warehouseCode", warehoseCodes);
         criteria.andIn("skuCode", skuCodes);
+        criteria.andEqualTo("isDelete", ZeroToNineEnum.ZERO.getCode());
         List<WarehouseItemInfo> warehouseItemInfoList = warehouseItemInfoService.selectByExample(example);
         AssertUtil.notEmpty(warehouseItemInfoList, String.format("发货单[%s]的相关商品全部不可用", CommonUtil.converCollectionToString(outboudOrderCodes)));
         return warehouseItemInfoList;
