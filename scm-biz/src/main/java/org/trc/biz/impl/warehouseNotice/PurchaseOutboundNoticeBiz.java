a31304220e006a683f4462c4f58c7f358218a52b
hzzyq <hzzyq@tairanchina.com>
2018-07-27 19:38:18 +0800

    1938

--- scm-biz/src/main/java/org/trc/biz/impl/warehouseNotice/PurchaseOutboundNoticeBiz.java
+++ scm-biz/src/main/java/org/trc/biz/impl/warehouseNotice/PurchaseOutboundNoticeBiz.java
@@ -1,6 +1,13 @@
 package org.trc.biz.impl.warehouseNotice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.trc.biz.warehouseNotice.IPurchaseOutboundNoticeBiz;
+import org.trc.domain.warehouseNotice.PurchaseOutboundNotice;
+import org.trc.form.warehouse.PurchaseOutboundNoticeForm;
+import org.trc.service.warehouseNotice.IPurchaseOutboundNoticeService;
+import org.trc.util.AssertUtil;
+import org.trc.util.Pagenation;
 
 /**
  * Description〈〉
@@ -8,5 +15,18 @@ import org.trc.biz.warehouseNotice.IPurchaseOutboundNoticeBiz;
  * @author hzliuwei
  * @create 2018/7/24
  */
+@Service("purchaseOutboundNoticeBiz")
 public class PurchaseOutboundNoticeBiz implements IPurchaseOutboundNoticeBiz {
+	
+	@Autowired
+	private IPurchaseOutboundNoticeService noticeService;
+
+	@Override
+	public Pagenation<PurchaseOutboundNotice> pageList(PurchaseOutboundNoticeForm form,
+			Pagenation<PurchaseOutboundNotice> page, String channelCode) {
+		AssertUtil.notNull(channelCode, "业务线编号不能为空");
+		return noticeService.pageList(form, page, channelCode);
+	}
+
+        
 }
--- scm-biz/src/main/java/org/trc/service/impl/warehouseNotice/PurchaseOutboundNoticeService.java
+++ scm-biz/src/main/java/org/trc/service/impl/warehouseNotice/PurchaseOutboundNoticeService.java
@@ -1,9 +1,23 @@
 package org.trc.service.impl.warehouseNotice;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+import org.trc.domain.allocateOrder.AllocateOrder;
+import org.trc.domain.impower.AclUserAccreditInfo;
 import org.trc.domain.warehouseNotice.PurchaseOutboundNotice;
+import org.trc.enums.ZeroToNineEnum;
+import org.trc.form.warehouse.PurchaseOutboundNoticeForm;
 import org.trc.service.impl.BaseService;
+import org.trc.service.impower.IAclUserAccreditInfoService;
 import org.trc.service.warehouseNotice.IPurchaseOutboundNoticeService;
+import org.trc.util.Pagenation;
+
+import tk.mybatis.mapper.entity.Example;
 
 /**
  * Description〈〉
@@ -11,6 +25,84 @@ import org.trc.service.warehouseNotice.IPurchaseOutboundNoticeService;
  * @author hzliuwei
  * @create 2018/7/25
  */
-@Service("purchaseOutboundNoticService")
-public class PurchaseOutboundNoticeService extends BaseService<PurchaseOutboundNotice,Long> implements IPurchaseOutboundNoticeService {
+@Service("purchaseOutboundNoticeService")
+public class PurchaseOutboundNoticeService extends BaseService<PurchaseOutboundNotice, Long> implements 
+	IPurchaseOutboundNoticeService {
+	
+	@Autowired
+	private IAclUserAccreditInfoService userInfoService;
+
+	@Override
+	public Pagenation<PurchaseOutboundNotice> pageList (PurchaseOutboundNoticeForm form,
+			Pagenation<PurchaseOutboundNotice> page, String channelCode) {
+		
+		Example example = new Example(AllocateOrder.class);
+        Example.Criteria criteria = example.createCriteria();
+        
+        criteria.andEqualTo("isDeleted", ZeroToNineEnum.ZERO.getCode());
+        //业务线编号
+        criteria.andEqualTo("channelCode", channelCode);
+        
+        //退货出库单号
+        if (StringUtils.isNotBlank(form.getOutboundNoticeCode())) {
+            criteria.andEqualTo("outboundNoticeCode", form.getOutboundNoticeCode());
+        }
+        
+        //采购退货单编号
+        if (StringUtils.isNotBlank(form.getPurchaseOutboundOrderCode())) {
+            criteria.andEqualTo("purchaseOutboundOrderCode", form.getPurchaseOutboundOrderCode());
+        }
+        
+        //退货仓库
+        if (StringUtils.isNotBlank(form.getWarehouseCode())) {
+        	criteria.andEqualTo("warehouseCode", form.getWarehouseCode());
+        }
+        
+        //供应商编号
+        if (StringUtils.isNotBlank(form.getSupplierId())) {
+        	criteria.andEqualTo("supplierId", form.getSupplierId());
+        }
+        
+        //出库单状态
+        if (StringUtils.isNotBlank(form.getStatus())) {
+        	criteria.andEqualTo("status", form.getStatus());
+        }
+        
+        //创建日期开始
+        if (StringUtils.isNotBlank(form.getStartDate())) {
+        	criteria.andGreaterThanOrEqualTo("createTime", form.getStartDate() + " 00:00:00");
+        }
+        
+        //创建日期结束
+        if (StringUtils.isNotBlank(form.getEndDate())) {
+        	criteria.andLessThanOrEqualTo("createTime", form.getEndDate() + " 23:59:59");
+        }
+        
+        //出库单创建人
+        if (StringUtils.isNotBlank(form.getCreateUser())) {
+            Example userExample = new Example(AclUserAccreditInfo.class);
+            Example.Criteria userCriteria = userExample.createCriteria();
+            
+            userCriteria.andLike("name", "%" + form.getCreateUser() + "%");
+            
+            List<AclUserAccreditInfo> userList = userInfoService.selectByExample(userExample);
+            if (CollectionUtils.isEmpty(userList)) {
+            	// 未查询到结果
+            	return new Pagenation<>();
+            } else {
+            	List<String> userIdList = new ArrayList<>();
+            	for (AclUserAccreditInfo user : userList) {
+            		userIdList.add(user.getUserId());
+            	}
+            	criteria.andIn("createOperator", userIdList);
+            }
+        }
+        
+        Pagenation<PurchaseOutboundNotice> pageResult = this.pagination(example, page, form);
+       // List<PurchaseOutboundNotice> result2 = result.getResult();
+        return pageResult;
+
+
+	}
+	
 }
