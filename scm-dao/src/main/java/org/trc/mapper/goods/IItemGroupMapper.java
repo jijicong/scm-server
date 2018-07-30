60fbd484445c54aa51f0f797754056945be5e666
hzguojl <JialiangguoVIP@163.com>
2018-07-30 17:30:55 +0800

    3.1

---
+++ scm-dao/src/main/java/org/trc/mapper/goods/IItemGroupMapper.java
@@ -0,0 +1,10 @@
+package org.trc.mapper.goods;
+
+import org.trc.domain.goods.ItemGroup;
+import org.trc.util.BaseMapper;
+
+/**
+ * Created by hzgjl on 2018/7/30.
+ */
+public interface IItemGroupMapper extends BaseMapper<ItemGroup> {
+}
