3fd9debe4e4057f752d6b549bca10b74b71f2141
hzguojl <JialiangguoVIP@163.com>
2018-07-31 19:15:32 +0800

    3.1

--- scm-biz/src/main/java/org/trc/form/goods/ItemGroupForm.java
+++ scm-biz/src/main/java/org/trc/form/goods/ItemGroupForm.java
@@ -1,9 +1,9 @@
 package org.trc.form.goods;
 
 import io.swagger.annotations.Api;
+import org.trc.domain.BaseDO;
 import org.trc.domain.goods.ItemGroup;
 import org.trc.domain.goods.ItemGroupUser;
-import org.trc.util.QueryModel;
 
 import java.util.List;
 
@@ -11,8 +11,8 @@ import java.util.List;
  * Created by hzgjl on 2018/7/31.
  */
 @Api("商品组新增/编辑提交表单数据")
-public class ItemGroupForm extends QueryModel {
-    private static final long serialVersionUID = 3204517750595228825L;
+public class ItemGroupForm extends BaseDO {
+    private static final long serialVersionUID = 3459115611247768853L;
 
    private ItemGroup itemGroup;
 
