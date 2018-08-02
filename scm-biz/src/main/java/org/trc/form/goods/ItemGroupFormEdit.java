f92d8332223a0261a40052b5e547389251c5956d
hzguojl <JialiangguoVIP@163.com>
2018-08-02 16:44:04 +0800

    Merge remote-tracking branch 'remotes/origin/v3.1' into dev
    # Conflicts:
    #	scm-web/src/main/java/org/trc/resource/ItemGroupResource.java

--- scm-biz/src/main/java/org/trc/form/goods/ItemGroupFormEdit.java
+++ scm-biz/src/main/java/org/trc/form/goods/ItemGroupFormEdit.java
@@ -1,12 +1,16 @@
 package org.trc.form.goods;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import javax.ws.rs.FormParam;
 
 /**
  * Created by hzgjl on 2018/8/2.
  */
+@Data
+@Api(value = "状态更新操作传值对象")
 public class ItemGroupFormEdit {
     @FormParam("isValid")
     @ApiModelProperty("启停用")
@@ -16,20 +20,4 @@ public class ItemGroupFormEdit {
     @FormParam("itemGroupCode")
     @ApiModelProperty("商品组编号")
     private String itemGroupCode;
-
-    public String getIsValid() {
-        return isValid;
-    }
-
-    public void setIsValid(String isValid) {
-        this.isValid = isValid;
-    }
-
-    public String getItemGroupCode() {
-        return itemGroupCode;
-    }
-
-    public void setItemGroupCode(String itemGroupCode) {
-        this.itemGroupCode = itemGroupCode;
-    }
 }
