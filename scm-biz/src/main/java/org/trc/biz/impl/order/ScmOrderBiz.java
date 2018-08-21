37cfe5388ea5e3913f887bef71eff5fec01b16e8
hzszy <876619304@qq.com>
2018-08-21 18:22:11 +0800

    京东代发库存查询

--- scm-biz/src/main/java/org/trc/biz/impl/order/ScmOrderBiz.java
+++ scm-biz/src/main/java/org/trc/biz/impl/order/ScmOrderBiz.java
@@ -3153,16 +3153,15 @@ public class ScmOrderBiz implements IScmOrderBiz {
     }
 
     @Override
-    public ResponseAck<List<StockNewResultVo>> getSkuStockQuery(String skuArray, String area) throws Exception {
+    public ResponseAck<List<StockNewResultDo>> getSkuStockQuery(String skuArray, String area) throws Exception {
         //参数校验
         ResponseAck responseAck = null;
-        checkSkuQuery(skuArray, area);
         //查询商品映射,将供应链的sku转换成京东的sku
         List<JdSkuStockQueryDO> jdSkuList = checkSkuQuery(skuArray, area);
         Set<String> skuIdSet = new HashSet<>();
         Map<String,String> skuMap = new HashMap<>();
         for (JdSkuStockQueryDO sku : jdSkuList) {
-            skuIdSet.add(sku.getSkuId());
+            skuIdSet.add(sku.getSkuCode());
         }
         if (!AssertUtil.collectionIsEmpty(skuIdSet)) {
             Example example = new Example(ExternalItemSku.class);
@@ -3174,30 +3173,30 @@ public class ScmOrderBiz implements IScmOrderBiz {
                 //获取无法识别的sku
                 List<String> errorSkuCode = getErrorSku(skuIdSet, itemSkuList);
                 //整理需要查询库存的数据
-                List<JdSkuStockQueryDO> skuStockQueryDOList = getJdSkuStockQueryDOList(jdSkuList, skuMap, itemSkuList);
+                List<JdExternalStockDO> skuStockQueryDOList = getJdSkuStockQueryDOList(jdSkuList, skuMap, itemSkuList);
                 //发送到external
                 JdSkuStockForm jdSkuStockForm = new JdSkuStockForm(skuStockQueryDOList, area);
                 ReturnTypeDO returnTypeDO = ijdService.getSkuStockQuery(jdSkuStockForm);
                 if (null != returnTypeDO.getResult()) {
                     JSONObject object = JSON.parseObject(returnTypeDO.getResult().toString());
                     String date = object.getString("data") == null ? "" : object.getString("data");
-                    List<StockNewResultVo> stockNewResultVoList;
-                    List<StockNewResultVo> resultVoList = new ArrayList<>();
+                    List<StockNewResultDo> stockNewResultDoList;
+                    List<StockNewResultVO> resultVoList = new ArrayList<>();
                     if (StringUtils.isNotBlank(date)) {
-                        stockNewResultVoList = JSON.parseArray(date, StockNewResultVo.class);
-                        for (StockNewResultVo resultVo : stockNewResultVoList) {
-                            if (StringUtils.isNotBlank(skuMap.get(resultVo.getSkuId()))) {
-                                StockNewResultVo newResultVo = new StockNewResultVo();
-                                BeanUtils.copyProperties(resultVo, newResultVo);
-                                newResultVo.setSkuId(skuMap.get(resultVo.getSkuId()));
+                        stockNewResultDoList = JSON.parseArray(date, StockNewResultDo.class);
+                        for (StockNewResultDo resultDo : stockNewResultDoList) {
+                            if (StringUtils.isNotBlank(skuMap.get(resultDo.getSkuId()))) {
+                                StockNewResultVO newResultVo = new StockNewResultVO();
+                                newResultVo = JSON.parseObject(JSON.toJSONString(resultDo),StockNewResultVO.class);
+                                newResultVo.setSkuCode(skuMap.get(resultDo.getSkuId()));
                                 resultVoList.add(newResultVo);
                             }
                         }
                     }
                     if (!AssertUtil.collectionIsEmpty(errorSkuCode)) {
                         for (String sku : errorSkuCode) {
-                            StockNewResultVo newResultVo = new StockNewResultVo();
-                            newResultVo.setSkuId(sku);
+                            StockNewResultVO newResultVo = new StockNewResultVO();
+                            newResultVo.setSkuCode(sku);
                             newResultVo.setStockStateId("222");
                             newResultVo.setStockStateDesc("sku无法识别");
                             resultVoList.add(newResultVo);
@@ -3211,18 +3210,20 @@ public class ScmOrderBiz implements IScmOrderBiz {
                 }
             }
 
+        }else {
+            responseAck = new ResponseAck(ResponseAck.SUCCESS_CODE, "库存查询成功", new ArrayList<>());
         }
         return responseAck;
     }
 
-    private List<JdSkuStockQueryDO> getJdSkuStockQueryDOList(List<JdSkuStockQueryDO> jdSkuList, Map<String, String> skuMap, List<ExternalItemSku> itemSkuList) {
-        List<JdSkuStockQueryDO> skuStockQueryDOList = new ArrayList<>();
+    private List<JdExternalStockDO> getJdSkuStockQueryDOList(List<JdSkuStockQueryDO> jdSkuList, Map<String, String> skuMap, List<ExternalItemSku> itemSkuList) {
+        List<JdExternalStockDO> skuStockQueryDOList = new ArrayList<>();
         for (ExternalItemSku itemSku : itemSkuList) {
             //sku键值对
             skuMap.put(itemSku.getSupplierSkuCode(),itemSku.getSkuCode());
-            JdSkuStockQueryDO stockQueryDO = new JdSkuStockQueryDO();
+            JdExternalStockDO stockQueryDO = new JdExternalStockDO();
             for (JdSkuStockQueryDO skuDo : jdSkuList) {
-                if (StringUtils.equals(itemSku.getSkuCode(), skuDo.getSkuId())) {
+                if (StringUtils.equals(itemSku.getSkuCode(), skuDo.getSkuCode())) {
                     stockQueryDO.setSkuId(itemSku.getSupplierSkuCode());
                     stockQueryDO.setNum(skuDo.getNum());
                     break;
