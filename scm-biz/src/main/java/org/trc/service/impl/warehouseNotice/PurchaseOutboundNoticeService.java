cd63159873a780eac5268452ad8e06b2e4545fb3
hzzyq <hzzyq@tairanchina.com>
2018-08-29 18:27:49 +0800

    order

--- scm-biz/src/main/java/org/trc/service/impl/warehouseNotice/PurchaseOutboundNoticeService.java
+++ scm-biz/src/main/java/org/trc/service/impl/warehouseNotice/PurchaseOutboundNoticeService.java
@@ -146,6 +146,7 @@ public class PurchaseOutboundNoticeService extends BaseService<PurchaseOutboundN
         
 		example.setOrderByClause("field(status," + PurchaseOutboundNoticeStatusEnum.TO_BE_NOTIFIED.getCode() +
 				"," + PurchaseOutboundNoticeStatusEnum.WAREHOUSE_RECEIVE_FAILED.getCode() + ") desc");
+		example.setOrderByClause("status in('0','2') desc");
         example.orderBy("createTime").desc();
         
         Pagenation<PurchaseOutboundNotice> pageResult = this.pagination(example, page, form);
