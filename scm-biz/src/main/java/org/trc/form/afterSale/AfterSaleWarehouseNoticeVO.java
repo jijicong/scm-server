2f14d376286c027a3129a71fd842fd38e8f833a4
wangjie <850018001@qq.com>
2018-09-03 10:11:10 +0800

    1

--- scm-biz/src/main/java/org/trc/form/afterSale/AfterSaleWarehouseNoticeVO.java
+++ scm-biz/src/main/java/org/trc/form/afterSale/AfterSaleWarehouseNoticeVO.java
@@ -2,6 +2,8 @@ package org.trc.form.afterSale;
 
 import java.util.List;
 
+import javax.ws.rs.FormParam;
+
 import org.trc.domain.afterSale.AfterSaleWarehouseNotice;
 import org.trc.domain.afterSale.AfterSaleWarehouseNoticeDetail;
 
@@ -17,6 +19,7 @@ public class AfterSaleWarehouseNoticeVO extends AfterSaleWarehouseNotice{
 	private static final long serialVersionUID = 1L;
 	
 	@ApiModelProperty(value="售后入库单详情")
+	@FormParam("warehouseNoticeDetailList")
 	public List<AfterSaleWarehouseNoticeDetail> warehouseNoticeDetailList;
 
 }
