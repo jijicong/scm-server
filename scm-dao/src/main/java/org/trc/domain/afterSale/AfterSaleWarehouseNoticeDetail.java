71982aa46ff57ed8093b7524100216a248b45e2a
wangjie <850018001@qq.com>
2018-09-03 10:23:33 +0800

    1

--- scm-dao/src/main/java/org/trc/domain/afterSale/AfterSaleWarehouseNoticeDetail.java
+++ scm-dao/src/main/java/org/trc/domain/afterSale/AfterSaleWarehouseNoticeDetail.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Table;
+import javax.ws.rs.FormParam;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -31,90 +32,105 @@ public class AfterSaleWarehouseNoticeDetail implements Serializable {
      * 主键
      */
     @ApiModelProperty(value="主键")
+    @FormParam("id")
 	private String id;
     /**
      * 订单编号
      */
     @ApiModelProperty(value="订单编号")
 	@Column(name="shop_order_code")
+    @FormParam("shopOrderCode")
 	private String shopOrderCode;
     /**
      * 入库单编号
      */
     @ApiModelProperty(value="入库单编号")
 	@Column(name="warehouse_notice_code")
+    @FormParam("warehouseNoticeCode")
 	private String warehouseNoticeCode;
 	/**
      * 渠道商品订单号
      */
     @ApiModelProperty(value="渠道商品订单号")
 	@Column(name="order_item_code")
+    @FormParam("orderItemCode")
 	private String orderItemCode;
     /**
      * skuCode
      */
     @ApiModelProperty(value="skuCode")
 	@Column(name="sku_code")
+    @FormParam("skuCode")
 	private String skuCode;
 	/**
      * 商品名称
      */
     @ApiModelProperty(value="商品名称")
 	@Column(name="sku_name")
+    @FormParam("skuName")
 	private String skuName;
 	/**
      * 条形码
      */
     @ApiModelProperty(value="条形码")
 	@Column(name="bar_code")
+    @FormParam("barCode")
 	private String barCode;
 	/**
      * 商品规格描述
      */
     @ApiModelProperty(value="商品规格描述")
 	@Column(name="spec_nature_info")
+    @FormParam("specNatureInfo")
 	private String specNatureInfo;
     /**
      * 拟退货数量
      */
     @ApiModelProperty(value="拟退货数量")
 	@Column(name="return_num")
+    @FormParam("returnNum")
 	private Integer returnNum;
 	/**
      * 总入库数量
      */
     @ApiModelProperty(value="总入库数量")
 	@Column(name="total_in_num")
+    @FormParam("totalInNum")
 	private Integer totalInNum;
     /**
      * 正品入库数量
      */
     @ApiModelProperty(value="正品入库数量")
 	@Column(name="in_num")
+    @FormParam("inNum")
 	private Integer inNum;
     /**
      * 残品入库数量
      */
     @ApiModelProperty(value="残品入库数量")
 	@Column(name="defective_in_num")
+    @FormParam("defectiveInNum")
 	private Integer defectiveInNum;
 	/**
      * 商品图片,多张图片逗号分隔
      */
     @ApiModelProperty(value="商品图片,多张图片逗号分隔")
 	@Column(name="picture")
+    @FormParam("picture")
 	private String picture;
     /**
      * 修改时间
      */
     @ApiModelProperty(value="修改时间")
 	@Column(name="update_time")
+    @FormParam("updateTime")
 	private Date updateTime;
     /**
      * 添加时间
      */
     @ApiModelProperty(value="添加时间")
 	@Column(name="create_time")
+    @FormParam("createTime")
 	private Date createTime;
 
 
