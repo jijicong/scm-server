b48f255b61a06f6134b4cf6fc2379ec9be8bacc6
吴东雄 <hzwdx@tairanchina.com>
2018-09-05 12:58:18 +0800

    退货入库单收货结果通知接口注释修改

--- scm-web/src/main/java/org/trc/resource/api/WmsApiResource.java
+++ scm-web/src/main/java/org/trc/resource/api/WmsApiResource.java
@@ -1,6 +1,10 @@
 package org.trc.resource.api;
 
 import com.alibaba.fastjson.JSON;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,7 @@ import org.trc.biz.allocateOrder.IAllocateOutOrderBiz;
 import org.trc.biz.outbuond.IOutBoundOrderBiz;
 import org.trc.biz.warehouseNotice.IWarehouseNoticeBiz;
 import org.trc.form.outbound.OutboumdWmsDeliverResponseForm;
+import org.trc.form.returnIn.ReturnInWmsResponseForm;
 import org.trc.form.wms.WmsAllocateOutInRequest;
 import org.trc.form.wms.WmsInNoticeRequest;
 import org.trc.util.ResultUtil;
@@ -25,6 +30,7 @@ import javax.ws.rs.core.Response;
  * 自营仓库
  * Created by hzcyn on 2018/5/22.
  */
+@Api(value = "wms仓库调用接口")
 @Component
 @Path("api/wmsApi")
 public class WmsApiResource {
@@ -84,13 +90,29 @@ public class WmsApiResource {
      * @return
      * @throws Exception
      */
+    @ApiOperation(value = "退货入库单收货结果通知接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "request", value = "{\n" +
+                    "    \"afterSaleCode\":\"发货单编号\",\n" +
+                    "    \"warehouseNoticeCode\":\"入库单编号\",\n" +
+                    "    \"recordRemark\":\"理货结果录入备注\",\n" +
+                    "    \"recordPicture\":\"理货结果上传图片, 多个图片路径用逗号分隔\",\n" +
+                    "    \"confirmRemark\":\"确认到货备注\",\n" +
+                    "    \"returnInDetailWmsResponseFormList\":[\n" +
+                    "        \"warehouseNoticeCode\":\"入库单编号\",\n" +
+                    "        \"skuCode\":\"skuCode\",\n" +
+                    "        \"inNum\":\"正品入库数量\",\n" +
+                    "        \"defectiveInNum\":\"残品入库数量\"\n" +
+                    "    ]\n" +
+                    "}", dataType = "String", required = true)
+    })
     @POST
     @Path("returnInOrderResultNotice")
     @Produces(MediaType.APPLICATION_JSON)
     public Response returnInOrderResultNotice(@FormParam("request") String request) throws Exception {
-        OutboumdWmsDeliverResponseForm req = JSON.parseObject(request, OutboumdWmsDeliverResponseForm.class);
+        ReturnInWmsResponseForm req = JSON.parseObject(request, ReturnInWmsResponseForm.class);
         try{
-            outBoundOrderBiz.orderOutResultNotice(req);
+
         }catch (Exception e){
             return ResultUtil.createfailureResult(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), e.getMessage());
         }
