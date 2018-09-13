1a38870166dfb92c34e27eed33f5c788f5ae34af
hzszy <876619304@qq.com>
2018-09-13 16:05:09 +0800

    取消发货通知

--- scm-biz/src/main/java/org/trc/biz/impl/afterSale/AfterSaleNoticeTaskBiz.java
+++ scm-biz/src/main/java/org/trc/biz/impl/afterSale/AfterSaleNoticeTaskBiz.java
@@ -1,32 +1,40 @@
 package org.trc.biz.impl.afterSale;
 
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.trc.biz.afterSale.IAfterSaleNoticeTaskBiz;
+import org.trc.biz.impl.config.LogInfoBiz;
 import org.trc.domain.afterSale.AfterSaleOrder;
 import org.trc.domain.afterSale.AfterSaleOrderDetail;
-import org.trc.enums.AfterSaleOrderEnum;
-import org.trc.enums.AfterSaleTypeEnum;
+import org.trc.enums.*;
+import org.trc.exception.AfterSaleException;
 import org.trc.form.afterSale.AfterSaleNoticeWmsForm;
+import org.trc.form.afterSale.AfterSaleNoticeWmsResultVO;
 import org.trc.service.afterSale.IAfterSaleOrderDetailService;
 import org.trc.service.afterSale.IAfterSaleOrderService;
+import org.trc.service.config.ILogInfoService;
 import org.trc.util.AssertUtil;
 import tk.mybatis.mapper.entity.Example;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service("afterSaleNoticeTaskBiz")
 public class AfterSaleNoticeTaskBiz implements IAfterSaleNoticeTaskBiz {
+    private Logger logger = LoggerFactory.getLogger(AfterSaleNoticeTaskBiz.class);
 
     @Autowired
     IAfterSaleOrderService afterSaleOrderService;
 
     @Autowired
     IAfterSaleOrderDetailService afterSaleOrderDetailService;
+
+    @Autowired
+    ILogInfoService logInfoService;
 
     @Override
     public void cancelSendOutGoods() {
@@ -39,8 +47,11 @@ public class AfterSaleNoticeTaskBiz implements IAfterSaleNoticeTaskBiz {
         //2.获取售后单详情
         if (!AssertUtil.collectionIsEmpty(afterSaleOrderList)) {
             Set<String> afterSaleCodeSet = new HashSet<>();
+            Map<String, AfterSaleOrder> afterSaleCodeMap = new HashMap<>();
             for (AfterSaleOrder afterSaleOrder : afterSaleOrderList) {
                 afterSaleCodeSet.add(afterSaleOrder.getAfterSaleCode());
+                AfterSaleOrder saleOrder = JSON.parseObject(JSON.toJSONString(afterSaleOrder), AfterSaleOrder.class);
+                afterSaleCodeMap.put(afterSaleOrder.getAfterSaleCode(), saleOrder);
             }
             //
             Example detailExample = new Example(AfterSaleOrderDetail.class);
@@ -56,9 +67,76 @@ public class AfterSaleNoticeTaskBiz implements IAfterSaleNoticeTaskBiz {
                     saleNoticeWmsFrom.setSkuCode(orderDetail.getSkuCode());
                     wmsFromList.add(saleNoticeWmsFrom);
                 }
+                //3.请求仓库
+                if (AssertUtil.collectionIsEmpty(wmsFromList)) {
+                    List<AfterSaleNoticeWmsResultVO> saleNoticeWmsResultVOList = afterSaleOrderService.deliveryCancelResult(wmsFromList);
+                    //更新对应的售后单状态并通知渠道
+                    for (AfterSaleOrder afterSaleOrder : afterSaleOrderList) {
+                        for (AfterSaleNoticeWmsResultVO wmsResultVO : saleNoticeWmsResultVOList) {
+                            if (StringUtils.equals(wmsResultVO.getAfterSaleCode(), afterSaleOrder.getAfterSaleCode())) {
+                                switch (wmsResultVO.getFlg()) {
+                                    case "1":
+                                        afterSaleOrder.setStatus(AfterSaleOrderEnum.AfterSaleOrderStatusEnum.STATUS_2.getCode());
+                                        break;
+                                    case "2":
+                                        afterSaleOrder.setStatus(AfterSaleOrderEnum.AfterSaleOrderStatusEnum.STATUS_IS_CANCELING.getCode());
+                                        break;
+                                    case "3":
+                                        afterSaleOrder.setStatus(AfterSaleOrderEnum.AfterSaleOrderStatusEnum.STATUS_IS_FAIL.getCode());
+                                        break;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                    //更新数据库
+                    try {
+                        int count = afterSaleOrderService.updateAfterSaleOrderList(afterSaleOrderList);
+                        if (count == 0) {
+                            String msg = "修改售后单状态" + JSON.toJSONString(afterSaleOrderList) + "操作失败";
+                            logger.error(msg);
+                            throw new AfterSaleException(ExceptionEnum.AFTER_SALE_ORDER_UPDATE_EXCEPTION, msg);
+                        } else {
+                            //记录日志
+                            recordLog(afterSaleCodeMap, afterSaleOrderList, saleNoticeWmsResultVOList);
+                            //推送取消发货通知
+                            pushAfterSaleState(saleNoticeWmsResultVOList);
+                        }
+                    } catch (Exception e) {
+                        logger.error("更新数据库异常");
+                    }
+                }
             }
         }
-        //3.请求仓库
+    }
+
+    private void pushAfterSaleState(List<AfterSaleNoticeWmsResultVO> saleNoticeWmsResultVOList) {
 
     }
+
+    private void recordLog(Map<String, AfterSaleOrder> afterSaleCodeMap, List<AfterSaleOrder> afterSaleOrderList, List<AfterSaleNoticeWmsResultVO> saleNoticeWmsResultVOList) {
+        Map<String,AfterSaleNoticeWmsResultVO> wmsResultVOMap = new HashMap<>();
+        for (AfterSaleNoticeWmsResultVO saleNoticeWmsResultVO:saleNoticeWmsResultVOList) {
+            wmsResultVOMap.put(saleNoticeWmsResultVO.getAfterSaleCode(),saleNoticeWmsResultVO);
+        }
+        for (AfterSaleOrder afterSaleOrder : afterSaleOrderList) {
+            //数据对比记录日志,失败的记录备注
+            AfterSaleOrder oldAfterSaleOrder = afterSaleCodeMap.get(afterSaleOrder.getAfterSaleCode());
+            //对比状态
+            if (null != oldAfterSaleOrder && (oldAfterSaleOrder.getStatus() != afterSaleOrder.getStatus())) {
+                if (afterSaleOrder.getStatus() == AfterSaleOrderEnum.AfterSaleOrderStatusEnum.STATUS_IS_FAIL.getCode()) {
+                    AfterSaleNoticeWmsResultVO saleNoticeWmsResultVO = wmsResultVOMap.get(afterSaleOrder.getAfterSaleCode());
+                    String remake = "";
+                    if (null != saleNoticeWmsResultVO) {
+                        remake = saleNoticeWmsResultVO.getMsg();
+                    }
+                    logInfoService.recordLog(afterSaleOrder, afterSaleOrder.getId(), LogInfoBiz.ADMIN_SIGN, "", remake, LogOperationEnum.UPDATE.getMessage());
+                } else {
+                    logInfoService.recordLog(afterSaleOrder, afterSaleOrder.getId(), LogInfoBiz.ADMIN_SIGN, "", "", LogOperationEnum.UPDATE.getMessage());
+                }
+            }
+        }
+
+    }
+
 }
