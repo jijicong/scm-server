636b08c60b58776f6bb629a38c4408e5c05d8613
liuwei <503451781@qq.com>
2018-09-26 18:00:15 +0800

    退货出库记录明细修改
    Signed-off-by: liuwei <503451781@qq.com>

--- scm-biz/src/main/java/org/trc/service/impl/warehouseNotice/PurchaseOutboundNoticeService.java
+++ scm-biz/src/main/java/org/trc/service/impl/warehouseNotice/PurchaseOutboundNoticeService.java
@@ -308,13 +308,17 @@ public class PurchaseOutboundNoticeService extends BaseService<PurchaseOutboundN
 		 String noticeCode = resp.getOutboundNoticeCode();
 		 String wmsCode = resp.getWmsEntryReturnNoticeCode();
 
+		PurchaseOutboundNotice notice = this.selectOneByEntryOrderCode(wmsCode);
+		if(!StringUtils.equals(notice.getStatus(), PurchaseOutboundNoticeStatusEnum.ON_WAREHOUSE_TICKLING.getCode())){
+			return;
+		}
+
 		 if (CANCELLED.equals(resp.getStatus())) { // 已取消
 
 			 this.updateByCode(PurchaseOutboundNoticeStatusEnum.CANCEL, null, noticeCode);
 			 detailService.updateByOrderCode(PurchaseOutboundNoticeStatusEnum.CANCEL, noticeCode);
 			 // 日志处理
 			 try {
-				 PurchaseOutboundNotice notice = this.selectOneByEntryOrderCode(wmsCode);
 	             WarehouseInfo warehouse = warehouseInfoService.selectOneByCode(notice.getWarehouseCode());
 		         logInfoService.recordLog(notice, notice.getId().toString(), warehouse.getWarehouseName(),
 		        		LogOperationEnum.ENTRY_RETURN_NOTICE_CANCEL.getMessage(), "京东后台取消", null);
@@ -373,7 +377,7 @@ public class PurchaseOutboundNoticeService extends BaseService<PurchaseOutboundN
 				 detailService.updateByDetailId(detailStatus, nowTime, actualQty, detail.getId());
 				 // 记录库存变动明细
 				 try {
-					 insertStockDetail(actualQty, detail, wmsCode);
+					 insertStockDetail(actualQty, detail, notice);
 				 } catch (Exception e) {
 					 logger.error("JD仓采购退货记录库存变动明细失败， 仓库反馈code:{}, e:", wmsCode, e);
 				 }
@@ -399,7 +403,6 @@ public class PurchaseOutboundNoticeService extends BaseService<PurchaseOutboundN
 
 			 // 日志处理
 			 try {
-				 PurchaseOutboundNotice notice = this.selectOneByEntryOrderCode(wmsCode);
 	             WarehouseInfo warehouse = warehouseInfoService.selectOneByCode(notice.getWarehouseCode());
 		         logInfoService.recordLog(notice, notice.getId().toString(), warehouse.getWarehouseName(),
 		        		LogOperationEnum.ENTRY_RETURN_NOTICE_FINISH.getMessage(), remark, null);
@@ -416,11 +419,10 @@ public class PurchaseOutboundNoticeService extends BaseService<PurchaseOutboundN
 
 	}
 
-	private PurchaseOutboundNotice insertStockDetail(long actualQty, PurchaseOutboundDetail detail, String wmsCode) {
+	private PurchaseOutboundNotice insertStockDetail(long actualQty, PurchaseOutboundDetail detail, PurchaseOutboundNotice notice) {
 
-		logger.info("JD采购出库记录库存变动明， 订单编号:{}", wmsCode);
+		logger.info("JD采购出库记录库存变动明， 订单编号:{}, skuCode:{}, 出库数量:{}", detail.getOutboundNoticeCode(), detail.getSkuCode(), actualQty);
 
-		PurchaseOutboundNotice notice = this.selectOneByEntryOrderCode(wmsCode);
 		JdStockOutDetail jdStockOutDetail = new JdStockOutDetail();
 		jdStockOutDetail.setWarehouseCode(notice.getWarehouseCode());
 		jdStockOutDetail.setOutboundOrderCode(notice.getOutboundNoticeCode());
